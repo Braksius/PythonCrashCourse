9141ba7de131b59ad67e60b43ae316922635aa40
Braksius <filippo.giannnini@gmail.com>
2019-08-05 06:49:23 +0200

    chapter_2

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
-print("HeSllo Python world!")
+message = """Introduction
+
+      This PEP contains the index of all Python Enhancement Proposals, known as PEPs. PEP numbers are assigned by the PEP editors, and once assigned are never changed[1]. The version control history[2] of the PEP texts represent their historical record.
+      Index by Category
+      Meta-PEPs(PEPs about PEPs or Processes)"""
+print(message)
