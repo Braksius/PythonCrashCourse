4924766978e6fa7b939e78fc40c67c102533b1be
Braksius <filippo.giannnini@gmail.com>
2019-08-09 21:29:41 +0200

    chapter_2

--- full_name.py
+++ full_name.py
@@ -1,4 +1,9 @@
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
-print(full_name)
+message = f"\tHello, {full_name.title()}!"
+print(message)
+message = "Languages:\n\tPython\n\tC\n\tJavaScript"
+print(message)
+favorite_language = "python "
+print(favorite_language.rstrip())
