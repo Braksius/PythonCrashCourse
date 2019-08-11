b249bfdb9ae1c802ad549c560d4abed3ec5fafb5
Braksius <filippo.giannnini@gmail.com>
2019-08-11 21:51:17 +0200

    chapter_3

---
+++ chapter_3/your_own_list.py
@@ -0,0 +1,5 @@
+transport = ['motocycle','car','train','bus']
+print(f"I would like a Honda {transport[0]}")
+print(f"I would like a Ferrari {transport[1]}")
+print(f"I would like a Honda {transport[2]}")
+print(f"I would like a Volvo {transport[3]}")
