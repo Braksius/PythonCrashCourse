7f2bcf348f176807d5466b0696cec6e0217b54db
Braksius <filippo.giannnini@gmail.com>
2019-08-11 21:45:57 +0200

    chapter_3

---
+++ chapter_3/greetings.py
@@ -0,0 +1,6 @@
+names = ['manuela','stefania','angelo','fabio']
+print(f"Welcome {names[0].title()}")
+print(f"Welcome {names[1].title()}")
+print(f"Welcome {names[2].title()}")
+print(f"Welcome {names[3].title()}")
+
