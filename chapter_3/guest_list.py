9ec7fdd59e3026912e2d1a25a6e012de812e6f60
Braksius <filippo.giannnini@gmail.com>
2019-08-11 22:11:08 +0200

    chapter_3

---
+++ chapter_3/guest_list.py
@@ -0,0 +1,4 @@
+guest_list = ['manuela','stefania','angelo']
+print(f"You are invite to my dinner {guest_list[0].title()}")
+print(f"You are invite to my dinner {guest_list[1].title()}")
+print(f"You are invite to my dinner {guest_list[2].title()}")
