db3cacc67474d6260bb417657c48747b7357e7c1
Braksius <filippo.giannnini@gmail.com>
2019-08-16 16:22:33 +0200

    chapter_4

---
+++ chapter_4/buffet.py
@@ -0,0 +1,7 @@
+foods = ('pizza','sandwich','pasta','hamburger','hot dog')
+for food in foods:
+    print(food.title())
+
+foods = ('salad','sandwich','chicken','hamburger','hot dog')
+for food in foods:
+    print(food.title())
