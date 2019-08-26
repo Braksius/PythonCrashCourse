dfac368b08995466bc1ef79a8356afb65622a1eb
Braksius <filippo.giannnini@gmail.com>
2019-08-26 21:15:39 +0200

    chapter_5

--- chapter_5/toppings.py
+++ chapter_5/toppings.py
@@ -1,4 +1,12 @@
-requested_topping = 'mushrooms'
+requested_toppings = ['mushrooms', 'extra cheese']
 
-if requested_topping != 'anchovies':
-    print("Hoeld the anchovies!")
+for requested_topping in requested_toppings:
+    if 'mushrooms' == requested_topping:
+        topping = 'mushrooms'
+    if 'pepperoni' == requested_topping:
+        topping = 'pepperoni'
+    if 'extra cheese' == requested_topping:
+        topping = 'extra cheese'
+    print(f"Adding {topping}.")
+
+print("\nFinished making your pizza!")
