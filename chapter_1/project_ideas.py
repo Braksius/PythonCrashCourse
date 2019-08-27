598db548c5793e86dea5a46c476ac59c4f983c86
Braksius <filippo.giannnini@gmail.com>
2019-08-27 07:24:32 +0200

    chapter_1

---
+++ chapter_1/project_ideas.py
@@ -0,0 +1,10 @@
+project_ideas = ['euro million statistics',
+                 'crawling amazon for best price',
+                 'asian adult site',
+                 'crawling betting site for statistics',
+                 'advanced IA algorithme'
+                 ]
+count = 1
+for project_idea in project_ideas:
+    print(f"\nProject {count} : {project_idea.title()}")
+    count = count+1
